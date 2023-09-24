17c67b872c95761afb4957850a2fa3ccee56b648
Mosio <hesariam@gmail.com>
2023-09-24 17:13:14 +0330

    funcitons update

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
 # LPICME
 LPIC Mock Exam
 
-To do:
-- a GUI python application for start and test
-	needs to be more responsive
-	clear the screen for each question
-- a data entry application for entering questions
-	needs to append answers to json
-	make another of for the questions on the book
+A CLI application 
--- exam.py
+++ exam.py
@@ -1,25 +1,57 @@
 import json
+import os
+
 
 def read_questions():
     with open("questions.json", "r") as file:
         return json.load(file)
 
-def start_quiz():
-    questions = read_questions()
-    score = 0
-    for question in questions:
-        print(question["question"])
-        for i, option in enumerate(question["options"]):
+
+def ask_question(qus):
+    print("Lenght of list of answers : ", len(qus['answer']))
+    answer_lenght = len(qus["answer"])
+    if answer_lenght == 1:
+        for i, option in enumerate(qus["options"]):
             print(str(i + 1) + ") " + option)
-        user_answer = input("Enter the number of your answer: ")
-        if question["options"][int(user_answer) - 1] == question["answer"]:
-            print("Correct!")
-            score += 1
+        user_answer = input(qus["question"])
+        return user_answer
+    else:
+        for i, option in enumerate(qus["options"]):
+            print(str(i + 1) + ") " + option)
+        print(f" *  Notice: Choose {answer_lenght} answers separated by ','  like: 2,3")
+        user_answer = input(qus["question"])
+        return user_answer
+
+
+def check_answer(qus, ans):
+    if len(qus['answer']) == 1:
+        if qus["options"][int(ans) - 1] == qus["answer"]:
+            print("correct")
+            return 1
         else:
-            print("Incorrect. ")
-            print("Answer is: ", question["answer"])
-        print(question["explanation"])
-    print("Your final score is " + str(score) + " out of " + str(len(questions)) + ".")
+            print("Incorrect")
+            return 0
+    else:
+        answers = ans.split(",")
+        print(answers)
+        for a in answers:
+            if qus["options"][int(a)-1] in qus['answer']:
+                print(qus["options"][int(a)-1])
+            else:
+                print('wrong')
 
-start_quiz()
 
+def explain(qus):
+    ...
+
+def quiz(questions):
+    for q in questions:
+        os.system('clear')
+        answer = ask_question(q)
+        check_answer(q, answer)
+        input("Press Enter to continue ...")
+
+
+if __name__ == "__main__":
+    questions = read_questions()
+    quiz(questions)
